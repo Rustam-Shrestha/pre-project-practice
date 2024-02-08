df88e9301a879e49ebe2ff9d219af77b19b514e2
Santosh <santosh.787402@smc.tu.edu.np>
2024-02-08 08:28:11 +0545

    decent looking funcitonal navbar is made here

--- components/header.php
+++ components/header.php
@@ -1,7 +1,7 @@
-<header>
+<header class="header">
     <div class="flex">
         <a href="home.php">
-            <img src="../images/image1.jpg" alt="linker" class="logo">
+            <img style="width:40px; height:40px" src="./images/image1.jpg" alt="linker" class="logo">
         </a>
         <nav class="navbar">
             <a href="home.php">home</a>
@@ -11,16 +11,19 @@
             <a href="contact.php">contact us</a>
         </nav>
         <div class="icons">
-            <i class="bx bxs-users" id="user-btn"></i>
-            <i class="bx bxs-users" id="user-btn"></i>
-            <a href="wishlist.php" class="cart-btn"> <i class="bx bs-heart"></i><sup>0</sup></a>
-            <a href="cart.php" class="cart-btn"> <i class="bx bs-cart-download"></i><sup>0</sup></a>
+            <i class="bx bx-user" id="user-btn"></i>
+            <a href="wishlist.php" class="cart-btn"> <i class="bx bx-heart"></i><sup>0</sup></a>
+            <a href="cart.php" class="cart-btn"> <i class="bx bx-cart-download"></i><sup>0</sup></a>
             <i class="bx-list-plus bx" id="menu-btn" style="font-size:2rem;"></i>
 
         </div>
         <div class="user-box">
-            <p>username: <span><?php $_SESSION['user_name']?></span></p>
-            <p>Email: <span><?php $_SESSION['user_email']?></span></p>
+            <p>username: <span>
+                    <!-- <?php $_SESSION['user_name'] ?> -->
+                </span></p>
+            <p>Email: <span>
+                    <!-- <?php $_SESSION['user_email'] ?> -->
+                </span></p>
             <a href="login.php" class="btn">Log in</a>
             <a href="register.php" class="btn">Register</a>
             <form method="post">
--- home.php
+++ home.php
@@ -2,20 +2,24 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
         <?php include "style.css"; ?>
-
     </style>
 </head>
+
 <body>
     <?php include "components/header.php"; ?>
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
     <script src="script.js"></script>
-<?php include "components/alert.php"; ?>
+    <?php include "components/alert.php"; ?>
 </body>
+
 </html>
