aa67201ca434b87b27be29d2cd64dd4f8c15e3de
Santosh <santosh.787402@smc.tu.edu.np>
2024-02-09 23:02:34 +0545

    added contact and about page with some rough stying and more to come with register form and backend and so forth

---
+++ about.php
@@ -0,0 +1,78 @@
+<?php include "components/connection.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        <?php include "style.css"; ?>
+    </style>
+</head>
+
+<body>
+    <?php include "components/header.php"; ?>
+    <div class="banner">
+        <h1>About us</h1>
+    </div>
+    <div class="title2">
+        <a href="home.php">home</a><span>/about</span>
+    </div>
+    <div class="about-category">
+        <div class="box">
+            <img src="./images/image3.jpg" alt="">
+            <div class="detail">
+                <span>fruit</span>
+                <h1>lemon</h1>
+                <a href="view_products.php" class="btn">shop now</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="./images/image4.jpg" alt="">
+            <div class="detail">
+                <span>fruit</span>
+                <h1>Raspberry</h1>
+                <a href="view_products.php" class="btn">shop now</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="./images/image2.jpg" alt="">
+            <div class="detail">
+                <span>fruit</span>
+                <h1>Mulberry</h1>
+                <a href="view_products.php" class="btn">shop now</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="./images/image1.jpg" alt="">
+            <div class="detail">
+                <span>fruit</span>
+                <h1>graves</h1>
+                <a href="view_products.php" class="btn">shop now</a>
+            </div>
+        </div>
+        
+    </div>
+    <div class="about">
+        <div class="row">
+            <div class="img-box">
+                <img src="./images/image1.jpg" alt="">
+            </div>
+            <div class="detail">
+                <h1>Lorem ipsum dolor sit amet.</h1>
+                <b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam reiciendis exercitationem quae quaerat? Vel, distinctio. Similique reiciendis enim iusto aspernatur distinctio illo voluptatum, aperiam delectus cupiditate ea.</b>
+                <a href="view_products.php" class="btn"></a>
+            </div>
+        </div>
+    </div>
+    <?php include "components/footer.php"; ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <script src="script.js"></script>
+    <?php include "components/alert.php"; ?>
+</body>
+
+</html>
---
+++ contact.php
@@ -0,0 +1,96 @@
+<?php include "components/connection.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        <?php include "style.css"; ?>
+    </style>
+</head>
+
+<body>
+    <?php include "components/header.php"; ?>
+    <div class="contact">
+        <div class="banner">
+            <h1>About us</h1>
+        </div>
+
+        <div class="title2">
+            <a href="home.php">home</a><span>/contact</span>
+        </div>
+        <div class="form-container">
+            <form action="" method="post">
+                <div class="title">
+                    <img src="./images/image3.jpg" alt="">
+                    <h1>leave a message</h1>
+                </div>
+                <div class="input-field">
+                    <p>Your name <sup>*</sup></p>
+                    <input type="text" name="name" id="">
+                </div>
+                <div class="input-field">
+                    <p>Your name <sup>*</sup></p>
+                    <input type="email" name="email" id="">
+                </div>
+                <div class="input-field">
+                    <p>Your name <sup>*</sup></p>
+                    <input type="text" name="number" id="">
+                </div>
+                <div class="input-field">
+                    <p>Your message <sup></sup></p>
+                    <textarea name="message" id="" cols="30" rows="10"></textarea>
+                </div>
+                <button class="btn" name="submit-button">send</button>
+
+            </form>
+            <div class="address">
+                <div class="title">
+                    <img src="./images/image3.jpg" alt="">
+                    <h1>contact details</h1>
+                </div>
+                <div class="box-container">
+                    <div class="box">
+                        <i class="bx bxs-map-pin"></i>
+                        <div>
+                            <h4>Address</h4>
+                            <p>Bishambar, Dhanusha</p>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <i class="bx bxs-phone-call"></i>
+                        <div>
+                            <h4>Phone no</h4>
+                            <p>+977 9869696969</p>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <i class="bx bxs-envelope"></i>
+                        <div>
+                            <h4>Email</h4>
+                            <p>santosh.787407@smc.tu.edu.np</p>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <i class="bx bxs-map-pin"></i>
+                        <div>
+                            <h4>Address</h4>
+                            <p>Bishambar, Dhanusha</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include "components/footer.php"; ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <script src="script.js"></script>
+    <?php include "components/alert.php"; ?>
+</body>
+
+</html>
