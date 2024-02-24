c766187e21a0924f28a281e6c2b4718535a99302
Santosh <santosh.787402@smc.tu.edu.np>
2024-02-24 18:13:20 +0545

    now a logged in users can only add products to their wishlist

--- view_products.php
+++ view_products.php
@@ -1,4 +1,41 @@
-<?php include "components/connection.php"; ?>
+<?php include "components/connection.php"; 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+session_start();
+if(isset($_SESSION['user_id'])){
+    $user_id= $_SESSION['user_id'];
+}else{
+    $user_id = "";
+}
+if(isset($_POST['logout'])){
+    session_destroy();
+    header("location: login.php");
+}
+// adding a product in wishlist
+if(isset($_POST['add_wishlist'])){
+    $id= uniq_poet();
+    $product_id=$_POST['product_id'];
+    $verify_wishlist = $con->prepare('SELECT * FROM `wishlist` WHERE user_id = ? AND product_id = ?');
+    $verify_wishlist->execute([$user_id, $product_id]);
+    $cart_num =  $con->prepare('SELECT * FROM `cart` WHERE user_id = ? AND product_id = ?');
+    $cart_num->execute([$user_id, $product_id]);
+    if($verify_wishlist->rowCount()>0){
+        $warning_msg[]= 'product already exists in your wishlist';
+        
+    }else if($cart_num->rowCount()>0){
+        $warning_msg[]= 'product already exists in your cart';
+        
+    }else{
+        $select_price = $con->prepare("SELECT * FROM `product` WHERE id= ? LIMIT 1");
+        $select_price->execute([$product_id]);
+        $fetch_price = $select_price->fetch(PDO::FETCH_ASSOC);
+        $insert_wishlist = $con->prepare("INSERT INTO `wishlist` (id, user_id, product_id, price) VALUES(?,?,?,?)");
+        $insert_wishlist->execute([$id, $user_id, $product_id, $fetch_price['price']]);
+        $success_msg[]= 'successfully added to cart';
+    }
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -33,26 +70,31 @@
                         ?>
                         <form action="" method="post" class="box">
 
-                        <img src="image/<?=$fetch_products['image'];?>" class='img' />
-                        <div class="button">
-                            
-                            <button type="submit" name="add_to_cart" > <i class="bx bx-cart"></i></button>
-                            <button type="submit" name="add_wishlist" > <i class="bx bx-heart"></i></button>
-                            <a href="viewpage.php?id=<?php echo $fetch_products['id'];?>" class="bx bxs-show"></a>
-                        </div>
-                        <h3 class="name"><?=$fetch_products['name'];?></h3>
-                        <input type="hidden" name="product_id">
-                        <div class="flex">
-                            <p class="price">price: <?=$fetch_products['price'];?></p>
-                            <input type="number" name="qty" required value="1" min="1" max="99" maxlength="2" class="qty">
-                        </div>
-                        <a href="checkout.php?get_id=<?=$fetch_products['id'];?>" class="btn">buy now</a>
-                        
+                            <img src="image/<?= $fetch_products['image']; ?>" class='img' />
+                            <div class="button">
+
+                                <button type="submit" name="add_to_cart"> <i class="bx bx-cart"></i></button>
+                                <button type="submit" name="add_wishlist" value="<?= $fetch_products['id']; ?>"> <i class="bx bx-heart"></i></button>
+
+                                <a href="viewpage.php?id=<?php echo $fetch_products['id']; ?>" class="bx bxs-show"></a>
+                            </div>
+                            <h3 class="name">
+                                <?= $fetch_products['name']; ?>
+                            </h3>
+                            <input type="hidden" name="product_id" value="<?= $fetch_products['id']; ?>">
+
+                            <div class="flex">
+                                <p class="price">price:
+                                    <?= $fetch_products['price']; ?>
+                                </p>
+                                <input type="number" name="qty" required value="1" min="1" max="99" maxlength="2" class="qty">
+                            </div>
+                            <a href="checkout.php?get_id=<?= $fetch_products['id']; ?>" class="btn">buy now</a>
+
                         </form>
                         <?php
                     }
-                }
-                else{
+                } else {
                     echo '<p class="empty">no products added yet! </p>';
                 }
                 ?>
